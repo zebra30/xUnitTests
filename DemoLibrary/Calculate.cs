0f491741da9f2c2d726c0a8b42348dcd82a5730c
Alex Klo <aaom@nni.net>
2018-03-25 13:37:55 -0500

    Initial Commit

---
+++ DemoLibrary/Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Calculate
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Mulitply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("y can not equal 0");
+            }
+
+            return x / y;
+        }
+    }
+}
---
+++ xUnitTestingProject/DemoTestCalculate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+
+namespace xUnitTestingProject
+{
+    public class DemoTestCalculate
+    {
+        [Fact]
+        public void Add_DemoCalculate()
+        {
+            //Arrange
+            int expected = 7;
+            Calculate calc = new Calculate();
+            //Act
+            int result = calc.Add(3, 4);
+            //Assert
+            Assert.Equal(expected, result);
+            
+        }
+
+        [Theory]
+        [InlineData(4, 5, 9)]
+        [InlineData(4, 5, 9)]
+        [InlineData(4, 1, 5)]
+        public void Add_DemoCalculateWithParameter(int x, int y, int expected)
+        {
+            //Arrange
+            Calculate calc = new Calculate();
+            //Act
+            int result = calc.Add(x, y);
+            //Assert
+            Assert.Equal(expected, result);
+           
+        }
+
+        [Fact]
+        public void Divide_DemoCalculate()
+        {
+            int expected = 7;
+            //Arrange
+            Calculate calc = new Calculate();
+            //Act
+            int result = calc.Divide(49, 7);
+            //Assert
+            Assert.Equal(expected, result);
+
+        }
+
+        [Fact]
+        public void Divide_DemoCalculateException()
+        {
+            //Arrange
+            Calculate calc = new Calculate();
+            //Assert
+            Assert.Throws<ArgumentException>(() => calc.Divide(49, 0));
+
+        }
+    }
+}
